e7ad25e02f693589b0d49b09d665d56c55e87a0d
Exequiel65 <marcos97britos@gmail.com>
2022-12-26 21:52:40 -0300

    Se agrego modelos Categoria y Marca

--- Entities/ApplicationDbContext.cs
+++ Entities/ApplicationDbContext.cs
@@ -10,6 +10,48 @@ namespace Entities
         }
 
         //Escribir modelos
-        public DbSet<Producto> Producto { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.ToTable("Producto");
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+                entity.Property(p => p.Precio)
+                    .HasColumnType("decimal(18,2)");
+
+                entity.HasOne(p => p.Marca)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(p => p.MarcaId);
+                entity.HasOne(p => p.Categoria)
+                    .WithMany(p => p.Productos)
+                    .HasForeignKey(p => p.CategoriaId);
+            });
+
+            modelBuilder.Entity<Marca>(entity =>
+            {
+                entity.ToTable("Marca");
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Categoria>(entity =>
+            {
+                entity.ToTable("Categoria");
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+        }
     }
 }
